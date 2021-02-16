f164a0e41aa2a79eea1d0daca2138b3e4aa207e0
Simonsoto <sa.soto.ochoa@gmail.com>
2021-02-16 18:09:25 +0100

    Update svm_author_id.py
    - Create classifier
    - Train the classifier
    - Calculate accuracy

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,32 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+#Import required packages
+from sklearn import svm
+from sklearn.scores import accuracy_score
+
+#Support vector classification
+clf = SVC(kernel ="linear")
+
+#Training SVC
+t0=time()
+clf.train(features_train, labels_train)
+
+print('Training time' + round(time()-t0,3)+ ' s.')
+
+
+
+#Accuracy of the classifier
+t1 = time()
+y_test = clf.predict(features_test)
+print("Predciting time:", round(time()-t1, 3), "s")
+
+
+#accuracy 
+accuracy_score(labels_test,y_test)
+
+
+
 #########################################################
 
 
